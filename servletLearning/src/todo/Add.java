1055a835e9feb193741c354df8573411ae5b90e4
hk4649 <hiroto.kawai@i-Vinci.co.jp>
2022-01-24 22:36:13 +0900

    ignore

--- servletLearning/src/todo/Add.java
+++ servletLearning/src/todo/Add.java
@@ -10,7 +10,6 @@ public class Add extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		request.setCharacterEncoding("UTF-8");
 		String name = request.getParameter("name");
-		System.out.println(name);
 		ModelList list = new ModelList();
 		boolean result = list.addDBData(name);
 		if (result) {
